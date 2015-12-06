27c80fdc64d70c49164206a8aa70a78d858e0f1c
Haisheng.Wu.Okta <haisheng.wu@okta.com>
2015-12-05 19:56:28 -0800

    wip - okta public API

--- chp-10/src/Hello/Ajax.purs
+++ chp-10/src/Hello/Ajax.purs
@@ -3,21 +3,50 @@
 module Hello.Ajax where
 
 import Prelude
---import Control.Monad.Eff
+import Data.Maybe
+import Control.Monad.Eff
 import Control.Monad.Eff.Class
-import Control.Monad.Eff.Console (log)
---import Control.Monad.Eff.Exception (EXCEPTION())
+import Control.Monad.Eff.Console (log, CONSOLE())
+import Control.Monad.Eff.Exception (EXCEPTION())
 import Control.Monad.Aff
 
 import Network.HTTP.Affjax
+import Network.HTTP.Affjax.Request
+import Network.HTTP.RequestHeader
 import Network.HTTP.Method
+
+import Config as C
 
 -- an public weather api
 -- http://api.openweathermap.org/data/2.5/weather?zip=94568,us&appid=2de143494c0b295cca9337e1e96b00e0
--- fetchWeather :: forall eff. Eff (ajax :: AJAX, err :: EXCEPTION | eff) Unit
+
+fetchWeather :: forall e. Eff (ajax :: AJAX, err :: EXCEPTION, console :: CONSOLE | e) Unit
 fetchWeather = launchAff $ do
   res <- affjax $ defaultRequest {
     url = "http://api.openweathermap.org/data/2.5/weather?zip=94568,us&appid=2de143494c0b295cca9337e1e96b00e0",
     method = GET
     }
   liftEff $ log $ "GET weather response: " ++ res.response
+
+
+fetchAppsUserType :: String
+                  -> forall e. Eff (ajax :: AJAX, err :: EXCEPTION, console :: CONSOLE | e) Unit
+fetchAppsUserType token = launchAff $ do
+  res <- affjax $ defaultReq { method = GET
+                             , url = C.domain + "/api/v1/users/me"
+                             }
+  liftEff $ log $ "GET app user types: " ++ res.response
+
+
+defaultReq :: AffjaxRequest Unit
+defaultReq =
+  { method: GET
+  , url: "/"
+  , headers: [ RequestHeader "Accept" "application/json"
+             , RequestHeader "ContentType" "application/json"
+             , RequestHeader "Authorization" ("SSWS " ++ C.token)
+             ]
+  , content: Nothing
+  , username: Nothing
+  , password: Nothing
+  }
--- chp-10/src/Main.purs
+++ chp-10/src/Main.purs
@@ -3,16 +3,28 @@ module Main where
 import Prelude
 import Data.List
 import Data.Foldable
+import Network.HTTP.Affjax (AJAX())
 import Control.Monad.Eff
+import Control.Monad.Eff.Console
+import Control.Monad.Eff.Exception (EXCEPTION())
 
 import Hello.Alert as A
 import Hello.Global as G
 import Hello.Ajax as AJ
+import Config as C
 
-main :: forall e. Eff (alert :: A.ALERT | e) Unit
-main = return unit
 
+main2 :: forall e. Eff (alert :: A.ALERT | e) Unit
+main2 = return unit
+
+main :: forall e. Eff (ajax :: AJAX, err :: EXCEPTION, console :: CONSOLE | e) Unit
+main = AJ.fetchAppsUserType C.token
+
+fetchWeather :: forall e. Eff (ajax :: AJAX, err :: EXCEPTION, console :: CONSOLE | e) Unit
 fetchWeather = AJ.fetchWeather
+
+appsUserType :: String -> forall e. Eff (ajax :: AJAX, err :: EXCEPTION, console :: CONSOLE | e) Unit
+appsUserType = AJ.fetchAppsUserType
 
 alert :: String -> forall e. Eff (alert :: A.ALERT | e) Unit
 alert = A.alert
